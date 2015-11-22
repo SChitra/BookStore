4c655d93426e3167981b0bc1a344276b7541c140
C S <sethuchitra@live.com>
2015-11-21 16:44:17 -0800

    BookStore intial code.

---
+++ BookStore/Book.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    /// <summary>
+    /// This class provides information about a book
+    /// </summary>
+    class Book
+    {
+      
+        #region Properties
+        public int Id { get; set; }
+        /// <summary>
+        /// Name of the Book
+        /// </summary>
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PubYear { get; set; }
+        /// <summary>
+        /// Number of Books
+        /// </summary>
+        public int Quantity { get; set; }
+        /// <summary>
+        /// Price of a book
+        /// </summary>
+        public decimal Price { get; set; }
+        #endregion
+
+        #region Method
+        /// <summary>
+        /// Adding books to the inventory
+        /// </summary>
+        /// <param name="Qty"></param>
+        /// <returns></returns>
+        public int Add(int Qty)
+        {
+            Quantity += Qty;
+            return Quantity;
+        }
+        /// <summary>
+        /// Decreases the Quantity from the available quantity
+        /// </summary>
+        /// <param name="Qty"></param>
+        /// <returns></returns>
+        public int Reduce(int Qty)
+        {
+            Quantity -= Qty;
+            return Quantity;
+        }
+        /// <summary>
+        /// Check the available Quantity
+        /// </summary>
+        /// <returns></returns>
+         public int CheckQuantity()
+        {
+            return Quantity;
+        }
+        /// <summary>
+        /// Selling the book and reduces the available quantity
+        /// </summary>
+        /// <param name="Qty"></param>
+        /// <returns></returns>
+        public decimal Sell(int Qty)
+        {
+            decimal totalPrice = Price * Qty;
+            Reduce(Qty);
+            return totalPrice;
+
+        }
+        #endregion
+    }
+
+
+}
---
+++ BookStore/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book myBook = new Book();
+            myBook.Id = 1;
+            myBook.Title = "Harry Potter";
+            myBook.Author = "ff";
+            myBook.PubYear = 2010;
+            myBook.Quantity = 5;
+            myBook.Price = 10;
+
+            int UpdatedQuantity = myBook.Add(2);
+            Console.WriteLine("Number of my Books available {0} ", myBook.CheckQuantity());
+
+            decimal totalCost = myBook.Sell(3);
+            Console.WriteLine("Sold Amount {0}", totalCost);
+            
+            Console.WriteLine("Number of my Books available  {0} ",myBook.CheckQuantity());
+           
+            Console.ReadLine();
+
+        }
+    }
+}
